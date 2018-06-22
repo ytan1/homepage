43cece2ba2014917501b056492f0cb7aca5c17c5
ytan1 <ytan1@ualberta.ca>
2018-06-22 14:29:34 -0600

    scrolling rendering

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 //smooth scrolling
-
+var html = document.documentElement
 var smoothScroll = function(e){
     var element = e.target || e.which;
     while(element !=null && element.tagName.toLowerCase() != 'a'){      //<a><div><svg>...</svg></div></a>   iterate to get a tag or null
@@ -17,7 +17,7 @@ var smoothScroll = function(e){
             if(target){
                 
                 e.preventDefault();
-                var html = document.getElementsByTagName('html')[0]; //body not work
+                // var html = document.getElementsByTagName('html')[0]; //body not work
                 var start = html.scrollTop;
                 var diff = target.offsetTop + target.offsetParent.offsetTop - start;  //target.offsetParent is div.content
                 //scroll 15px for 5ms
@@ -43,6 +43,11 @@ var smoothScroll = function(e){
 }
 document.getElementsByClassName('nav')[0].onclick = function(e){ smoothScroll(e) }
 document.getElementsByClassName('header-bottom')[0].onclick = function(e){ smoothScroll(e) }
+
+
+
+
+
 
 
 //insert animation to title
@@ -79,7 +84,7 @@ gameContainer.onclick = function(){
     game.start()
 
 }
-
+//scrolling down arrow in the header bottom
 var arrowDown = document.getElementsByClassName('arrow-down')[0]
 var headBottomAnimation = bodymovin.loadAnimation({
     container: arrowDown,
@@ -92,14 +97,84 @@ var headBottomAnimation = bodymovin.loadAnimation({
 
 
 
-//for nav bar scrolling
+//for nav bar scrolling, parallax scrolling and (fake) rendering after scroll
+var lastScrollTop = 0
+window.onload = function(){
+    lastScrollTop = window.pageYOffset || html.scrollTop
+}
 var nav = document.getElementsByClassName('nav')[0]
+var background = document.getElementsByClassName('background')[0]
+var scrollTop = 0
+var clientHeight = 0
+var scrollHeight = document.documentElement.scrollHeight;
+var contents = document.getElementsByClassName('content-block')
 window.onscroll = function(){
-    console.log('test')
-    if(document.documentElement.scrollTop > 784){
-        nav.classList.add('fix-nav')
+    scrollTop =  window.pageYOffset || html.scrollTop
+    clientHeight = html.clientHeight
+    if(scrollTop > 784){
+        nav.classList.add('fix-nav')        //fix nav after scrolling down to first content
     }
     else{
-        nav.classList.remove('fix-nav')
+        nav.classList.remove('fix-nav')     //unfix it
     }
+    background.style.top = 0.5*scrollTop + 'px'        //parallax scrolling
+    background.style.height = scrollHeight - 0.5*scrollTop + 'px'   //parallax scrolling set the background height
+
+    console.log(clientHeight)
+    //render content block when scrolling into
+    if(scrollTop > lastScrollTop){
+        //scrolling down 
+
+        Array.prototype.forEach.call(contents, function(ele){
+            if((scrollTop + clientHeight - 100) > (ele.offsetTop + ele.offsetParent.offsetTop)){
+                if(!ele.classList.contains('fade-in')){
+                    
+                    ele.classList.add('fade-in')
+                }
+            }
+            if(scrollTop + 100 > (ele.offsetTop + ele.offsetParent.offsetTop + ele.offsetHeight)){
+                if(ele.classList.contains('fade-in')){
+                    ele.classList.remove('fade-in')
+                }
+            }
+        })
+    }else{
+        //scrolling up
+        Array.prototype.forEach.call(contents, function(ele){
+            //the order of two if blocks must not be changed !!!
+            if(scrollTop + 100 < (ele.offsetTop + ele.offsetParent.offsetTop + ele.offsetHeight)){
+                if(!ele.classList.contains('fade-in')){
+                    ele.classList.add('fade-in')
+                }
+            }
+            if((scrollTop + clientHeight - 100) < (ele.offsetTop + ele.offsetParent.offsetTop)){
+                if(ele.classList.contains('fade-in')){
+                    ele.classList.remove('fade-in')
+                }
+            }
+        })
+    }
+
+
+
+
+
+
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+}
+
+
+
+
+
+
+
+//contact ejection
+var contacts = document.getElementsByClassName('nav-item')[3]
+var contactItem = document.getElementsByClassName('contact-item')
+contacts.onclick = function(){
+    Array.prototype.forEach.call(contactItem, function(ele){
+        ele.classList.toggle('eject')
+    })
 }
